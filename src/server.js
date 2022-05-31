94517e64dcae1c37df650136a1715f73ed6cd987
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-06-01 01:29:46 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import express from "express";
-
-const app = express();
+import morgan from "morgan";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} ☞☞`);
-const handleHome = (req,res) => {
+const app = express();
+const logger = morgan("dev");
+
+const home = (req,res) => {
     return res.send("I still love you");
 }
-const handleLogin = (req,res) => {
+const login = (req,res) => {
     return res.send("login here");
 }
 
+app.use(logger);
+app.get("/", home);
+app.get("/login",login);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} ☞☞`);
+
 app.listen(PORT, handleListening);
-app.get("/", handleHome);
-app.get("/login",handleLogin);
