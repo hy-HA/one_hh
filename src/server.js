c2f7a68bf3feb2b40fced0beee482ffa892b0353
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-06-02 00:35:49 +0900

    router

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
+
+
 
 const home = (req,res) => {
     return res.send("I still love you");
@@ -13,7 +19,10 @@ const login = (req,res) => {
     return res.send("login here");
 }
 
-app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 app.get("/", home);
 app.get("/login",login);
 
