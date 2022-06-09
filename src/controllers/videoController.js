5e85744e6e4a5c7a6fceedce6a4033d659c618b0
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-06-10 00:44:05 +0900

    Model & db connect

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,53 +1,21 @@
-const fakeUser = {
-    username: "Nicolas",
-    loggedIn: true,
-}
+import Video from "../models/Video";
 
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3, 
-  },
-];
-
-export const trending = (req, res) => {
+export const home = async (req, res) => {
+    const videos = await Video.find({});
     return res.render("home", {pageTitle : "Home", videos});
 }
 export const watch = (req, res) => {
     const {id} = req.params;
-    const video = videos[id-1];
-    return res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+    return res.render("watch", {pageTitle: "Watching"});
 }
 export const getEdit = (req, res) => {
     const {id} = req.params;
-    const video = videos[id-1];
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle: "Editing:"});
 }
 
 export const postEdit = (req, res) => {
   const {id} = req.params;
   const {title} = req.body;
-  videos[id-1].title = title;
   return res.redirect(`/videos/${id}`);
 }
 export const search = (req, res) => res.send("Search");
